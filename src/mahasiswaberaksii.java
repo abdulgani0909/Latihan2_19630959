03cf157f550522ce1425aa0ce2f09f07e3b044a3
abdulgani0909 <136442667+abdulgani0909@users.noreply.github.com>
2023-08-06 21:38:06 +0800

    Add files via upload
    melengkapi class mahasiswa

--- src/mahasiswaberaksii.java
+++ src/mahasiswaberaksii.java
@@ -1,23 +1,28 @@
+
+import universitas.Mahasiswa;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package universitas;
 
 /**
  *
  * @author user
  */
+public class mahasiswaberaksii {
 
+    /**
+     * @param args the command line arguments
+     */
 
-public class MahasiswaBeraksi {
     public static void main(String[] args) {
         // Membuat objek mahasiswa1 dari class Mahasiswa dengan constructor
         Mahasiswa mahasiswa1 = new Mahasiswa("19630959", "abdul gani", "Teknik Informatika");
 
         // Menggunakan setter untuk mengubah nilai npm, nama, dan prodi
-        mahasiswa1.setNpm("19630959");
+        mahasiswa1.setNpm("1630959");
         mahasiswa1.setNama("abdul gani");
         mahasiswa1.setProdi("teknik informatika");
 
@@ -25,5 +30,4 @@ public class MahasiswaBeraksi {
         System.out.println(mahasiswa1.getDetail());
     }
 }
-
-
+ 
--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -9,6 +9,52 @@ package universitas;
  *
  * @author user
  */
-public class mahasiswa {
-    
+
+
+public class Mahasiswa {
+    private String npm;
+    private String nama;
+    private String prodi;
+
+    // Constructor dengan parameter npm, nama, dan prodi
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    // Getter npm
+    public String getNpm() {
+        return npm;
+    }
+
+    // Setter npm
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    // Getter nama
+    public String getNama() {
+        return nama;
+    }
+
+    // Setter nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    // Getter prodi
+    public String getProdi() {
+        return prodi;
+    }
+
+    // Setter prodi
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public String getDetail() {
+        return "NPM: " + npm + ", Nama: " + nama + ", Prodi: " + prodi;
+    }
 }
+
